1c9b6cf2fa8b8ca6ba8b5542f076ba8e235fd52d
枯叶_听海 <5490584810@163.com>
2017-04-07 15:52:36 +0800

    修正五十音部分代码

--- src/main/java/com/jeremie/springfx/JapaneseSyllabary.java
+++ src/main/java/com/jeremie/springfx/JapaneseSyllabary.java
@@ -1,5 +1,6 @@
 package com.jeremie.springfx;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,72 +8,83 @@ import java.util.List;
  * @author guanhong 2017/3/27.
  */
 public enum JapaneseSyllabary {
-    あ("a", "あ", "ア"),
-    い("i", "い", "イ"),
-    う("u", "う", "ウ"),
-    え("e", "え", "エ"),
-    お("o", "お", "オ"),
+    a("a", "あ", "ア", "a.wav"),
+    i("i", "い", "イ", "i.wav"),
+    u("u", "う", "ウ", "u.wav"),
+    e("e", "え", "エ", "e.wav"),
+    o("o", "お", "オ", "o.wav"),
 
-    か("ka", "か", "カ"),
-    き("ki", "き", "キ"),
-    く("ku", "く", "ク"),
-    け("ke", "け", "ケ"),
-    こ("ko", "こ", "コ"),
+    ka("ka", "か", "カ", "ka.wav"),
+    ki("ki", "き", "キ", "ki.wav"),
+    ku("ku", "く", "ク", "ku.wav"),
+    ke("ke", "け", "ケ", "ke.wav"),
+    ko("ko", "こ", "コ", "ko.wav"),
 
-    さ("sa", "さ", "サ"),
-    し("si", "し", "シ"),
-    す("su", "す", "ス"),
-    せ("se", "せ", "セ"),
-    そ("so", "そ", "ソ"),
+    sa("sa", "さ", "サ", "sa.wav"),
+    si("si", "し", "シ", "si.wav"),
+    su("su", "す", "ス", "su.wav"),
+    se("se", "せ", "セ", "se.wav"),
+    so("so", "そ", "ソ", "so.wav"),
 
-    た("ta", "た", "タ"),
-    ち("chi", "ち", "チ"),
-    つ("tsu", "つ", "ツ"),
-    て("te", "て", "テ"),
-    と("to", "と", "ト"),
+    ta("ta", "た", "タ", "ta.wav"),
+    chi("chi", "ち", "チ", "chi.wav"),
+    tsu("tsu", "つ", "ツ", "tsu.wav"),
+    te("te", "て", "テ", "te.wav"),
+    to("to", "と", "ト", "to.wav"),
 
-    な("na", "な", "ナ"),
-    に("ne", "に", "ニ"),
-    ぬ("nu", "ぬ", "ヌ"),
-    ね("ne", "ね", "ネ"),
-    の("no", "の", "ノ"),
+    na("na", "な", "ナ", "na.wav"),
+    ni("ni", "に", "ニ", "ni.wav"),
+    nu("nu", "ぬ", "ヌ", "nu.wav"),
+    ne("ne", "ね", "ネ", "ne.wav"),
+    no("no", "の", "ノ", "no.wav"),
 
-    は("ha", "は", "ハ"),
-    ひ("hi", "ひ", "ヒ"),
-    ふ("fu", "ふ", "フ"),
-    へ("he", "へ", "ヘ"),
-    ほ("ho", "ほ", "ホ"),
+    ha("ha", "は", "ハ", "ha.wav"),
+    hi("hi", "ひ", "ヒ", "hi.wav"),
+    fu("fu", "ふ", "フ", "fu.wav"),
+    he("he", "へ", "ヘ", "he.wav"),
+    ho("ho", "ほ", "ホ", "ho.wav"),
 
-    ま("ma", "ま", "マ"),
-    み("mi", "み", "ミ"),
-    む("mu", "む", "ム"),
-    め("me", "め", "メ"),
-    も("mo", "も", "モ"),
+    ma("ma", "ま", "マ", "ma.wav"),
+    mi("mi", "み", "ミ", "mi.wav"),
+    mu("mu", "む", "ム", "mu.wav"),
+    me("me", "め", "メ", "me.wav"),
+    mo("mo", "も", "モ", "mo.wav"),
 
-    や("ya", "や", "ヤ"),
-    ゆ("yu", "ゆ", "ユ"),
-    よ("yo", "よ", "ヨ"),
+    ya("ya", "や", "ヤ", "ya.wav"),
+    i_y("i", "い", "イ", "i(y).wav"),
+    yu("yu", "ゆ", "ユ", "yu.wav"),
+    e_y("e", "え", "エ", "e(y).wav"),
+    yo("yo", "よ", "ヨ", "yo.wav"),
 
-    ら("ra", "ら", "ラ"),
-    り("ri", "り", "リ"),
-    る("ru", "る", "ル"),
-    れ("re", "れ", "レ"),
-    ろ("ro", "ろ", "ロ"),
+    ra("ra", "ら", "ラ", "ra.wav"),
+    ri("ri", "り", "リ", "ri.wav"),
+    ru("ru", "る", "ル", "ru.wav"),
+    re("re", "れ", "レ", "re.wav"),
+    ro("ro", "ろ", "ロ", "ro.wav"),
 
-    わ("wa", "わ", "ワ"),
-    を("o", "を", "ヲ"),//wo
+    wa("wa", "わ", "ワ", "wa.wav"),
+    i_w("i", "い", "イ", "i(w).wav"),
+    u_w("u", "う", "ウ", "u(w).wav"),
+    e_w("e", "え", "エ", "w(w).wav"),
+    o_w("o", "を", "ヲ", "o(w).wav"),//wo
 
-    ん("n", "ん", "ン");
-
+    n("n", "ん", "ン", "n.wav");
 
     private String phonetic;
     private String hiragana;
     private String katakana;
+    private String type;
+    private URL pronunciation;
 
-    JapaneseSyllabary(String phonetic, String hiragana, String katakana) {
+    private URL getURL(String fileName) {
+        return JapaneseSyllabary.class.getResource("/pronunciation/" + fileName);
+    }
+
+    JapaneseSyllabary(String phonetic, String hiragana, String katakana, String pronunciationFileName) {
         this.phonetic = phonetic;
         this.hiragana = hiragana;
         this.katakana = katakana;
+        this.pronunciation = getURL(pronunciationFileName);
     }
 
     public String getPhonetic() {
@@ -85,6 +97,10 @@ public enum JapaneseSyllabary {
 
     public String getKatakana() {
         return this.katakana;
+    }
+
+    public URL getPronunciation() {
+        return this.pronunciation;
     }
 
     public static JapaneseSyllabary[] getSyllabarys() {
